3ce233d844d2f00fcf13d51a4ea1df2258095ec5
webdev <webdeb@gmail.com>
2020-04-20 18:17:36 +0530

    'mod1_solution'

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -2,12 +2,37 @@
 'use strict';
 
 angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
 
-.controller('LunchCheckController', LunchCheckController {
-  LunchCheckController.$inject = ['$scope'];
-  function LunchCheckController($scope) {
-      $scope.state = "hungry";
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.dishes = "";
+  $scope.message = "";
+
+
+  $scope.lunchCheck = function () {
+    $scope.message = calculateDishes($scope.dishes);
+
+  };
+
+  function calculateDishes(string){
+    var ans = "";
+    var words ="";
+    words = string.split(',');
+
+    if(words.length<=3){
+      ans = "Enjoy!";
+    }
+    else if(words.length>3){
+      ans = "Too much!";
+    }
+    else{
+      ans = "Please enter data first";
+    }
+
+    return ans;
   }
-});
+
+}
 
 })();
